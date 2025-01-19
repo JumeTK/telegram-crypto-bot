aaa26ec3713130a0b55376a948f98df67a90378c
JumeTK <128575565+JumeTK@users.noreply.github.com>
2025-01-19 17:02:59 +0300

    Added new file

--- api/index.js
+++ api/index.js
@@ -52,14 +52,16 @@ async function fetchAndPostPrices() {
     }
 }
 
+// Fetch and post crypto prices every minute (60000ms)
+setInterval(fetchAndPostPrices, 60000);
+
+// Initially post the prices as soon as the bot starts
+fetchAndPostPrices();
+
 // Define the Vercel serverless function handler
 module.exports = async (req, res) => {
    await fetchAndPostPrices();
    res.status(200).send("Bot is running.");
 };
 
-// // Fetch and post crypto prices every minute (60000ms)
-// setInterval(fetchAndPostPrices, 60000);
 
-// // Initially post the prices as soon as the bot starts
-// fetchAndPostPrices();
